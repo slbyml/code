76335fec1ef058963448a616b4c838278dd3e75a
shaolibao <shaolibao@renrenche.com>
2019-03-30 17:15:25 +0800

    ast

---
+++ webpack/mywebpack/ast/index.js
@@ -0,0 +1,23 @@
+const parseAst = require('@babel/parser')
+const traverse = require('@babel/traverse').default
+const t = require('@babel/types')
+const generator = require('@babel/generator').default
+
+const code = 'const a = (b) => 123 + b'
+const ast = parseAst.parse(code)
+
+traverse(ast, {
+    VariableDeclaration(p) {
+        if (p.node.kind === 'const') {
+            p.node.kind = 'var'
+        }
+    },
+    ArrowFunctionExpression(p) {
+        const node = p.node
+        const returnB = t.returnStatement(node.body)
+        var block = t.blockStatement([returnB])
+        p.replaceWith(t.functionExpression(null, node.params, block, false, false))
+    }
+})
+
+console.log(generator(ast).code);
