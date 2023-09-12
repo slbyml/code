5402aec675bdfb8b4b972b71ccfe418c0df72ff1
邵礼豹 <shaolibao@xiaomi.com>
2023-09-12 14:06:16 +0800

    react

---
+++ react-base/react/react/packages/react/index.experimental.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+export {
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
+  act as unstable_act,
+  Children,
+  Component,
+  Fragment,
+  Profiler,
+  PureComponent,
+  StrictMode,
+  Suspense,
+  cloneElement,
+  createContext,
+  createElement,
+  createFactory,
+  createRef,
+  createServerContext,
+  use,
+  forwardRef,
+  isValidElement,
+  lazy,
+  memo,
+  cache,
+  startTransition,
+  unstable_Cache,
+  unstable_DebugTracingMode,
+  unstable_Offscreen,
+  unstable_postpone,
+  unstable_getCacheSignal,
+  unstable_getCacheForType,
+  unstable_SuspenseList,
+  unstable_useCacheRefresh,
+  unstable_useMemoCache,
+  useId,
+  useCallback,
+  useContext,
+  useDebugValue,
+  useDeferredValue,
+  useEffect,
+  experimental_useEffectEvent,
+  useImperativeHandle,
+  useInsertionEffect,
+  useLayoutEffect,
+  useMemo,
+  experimental_useOptimistic,
+  useReducer,
+  useRef,
+  useState,
+  useSyncExternalStore,
+  useTransition,
+  version,
+} from './src/React';
